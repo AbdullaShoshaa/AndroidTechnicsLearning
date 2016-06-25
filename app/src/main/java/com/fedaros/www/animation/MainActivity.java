5e3afb6d250d679fe7a80ad6d4f01aa3baddff47
Abdulla <abdulla.shoshaa@gmail.com>
2016-06-25 04:08:42 +0300

    animation using rotation method

--- app/src/main/java/com/fedaros/www/animation/MainActivity.java
+++ app/src/main/java/com/fedaros/www/animation/MainActivity.java
@@ -3,6 +3,8 @@ package com.fedaros.www.animation;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,17 +17,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         bart = (ImageView) findViewById(R.id.bartimageView);
-        //you can set bart ImageView to be out of the screen and then animate it to be visible.
-        bart.setTranslationX(-1000f);
-        bart.animate().translationX(0f).setDuration(2000);
+
     }
 
-    //translate method to translate bart Image.
-    public void translate(View view){
-        //animate bart using translate
-        //bart.animate().translationY(10000f).setDuration(2000);
-        //translate to left.
-        bart.animate().translationX(-10000f).setDuration(2000);
+
+    public void rotate(View view){
+
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                //int counter = 0;
+                //bart.animate().rotation(counter+=50).setDuration(500);
+                //create animation object and pass the required arguments.
+                Animation animation = new RotateAnimation(360, 0);
+                animation.setRepeatCount(Animation.INFINITE);
+                animation.setDuration(1000);
+                //set the animation object reference to imageView and start the animation effect.
+                bart.startAnimation(animation);
+
+            }
+        });
+
 
     }
 
