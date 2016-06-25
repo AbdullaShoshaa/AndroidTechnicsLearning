8f3e47d02da5cbfb0f54a319b2f93a855e9d9aaa
Abdulla <abdulla.shoshaa@gmail.com>
2016-06-25 03:40:47 +0300

    animation using Translate method

--- app/src/main/java/com/fedaros/www/animation/MainActivity.java
+++ app/src/main/java/com/fedaros/www/animation/MainActivity.java
@@ -7,38 +7,26 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    //instance variables for ImageView that hold homer and bart images
-    ImageView bart, homer;
+    //instance variables for ImageView that bart images
+    ImageView bart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bart = (ImageView) findViewById(R.id.bartimageView);
-        homer = (ImageView) findViewById(R.id.homer);
+        //you can set bart ImageView to be out of the screen and then animate it to be visible.
+        bart.setTranslationX(-1000f);
+        bart.animate().translationX(0f).setDuration(2000);
     }
 
-    //fade method to fade out bart Image and show homer image
-    public void fade(View view){
-        //animate bart to alpha 0 (disappear)
-        bart.animate().alpha(0f).setDuration(2000);
-        //set bart ImageView to gone so it won't interfere with homer ImageView while clicking.
-        bart.setVisibility(View.GONE);
-        //set homer ImageView to visible.
-        homer.setVisibility(View.VISIBLE);
-        //animate homer image to show up using alpha 1f
-        homer.animate().alpha(1f).setDuration(2000);
+    //translate method to translate bart Image.
+    public void translate(View view){
+        //animate bart using translate
+        //bart.animate().translationY(10000f).setDuration(2000);
+        //translate to left.
+        bart.animate().translationX(-10000f).setDuration(2000);
+
     }
 
-    //fade2 method to fade out homer Image and show bart image.
-    public void fade2(View view){
-        //animate homer to alpha 0
-        homer.animate().alpha(0f).setDuration(2000);
-        //set homer ImageView to gone so it won't interfere with bart ImageView while clicking.
-        homer.setVisibility(View.GONE);
-        //set bart imageview to visible.
-        bart.setVisibility(View.VISIBLE);
-        //animate bart to show up using alpha 1f
-        bart.animate().alpha(1f).setDuration(2000);
-    }
 }
